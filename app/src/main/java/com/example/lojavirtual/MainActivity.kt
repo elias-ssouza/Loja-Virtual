308c8492198535aa5784e13ca2f0463c43b7d374
elias-ssouza <elias.ssouza@hotmail.com>
2022-10-27 13:59:19 -0300

    atualização navigation na main activity

--- app/src/main/java/com/example/lojavirtual/MainActivity.kt
+++ app/src/main/java/com/example/lojavirtual/MainActivity.kt
@@ -3,17 +3,38 @@ package com.example.lojavirtual
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.Navigation
 import com.example.lojavirtual.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
+    lateinit var toolbar: Toolbar
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navigationView: NavigationView
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         insertListeners()
+
+        toolbar = findViewById(R.id.toolbar)
+        drawerLayout = findViewById(R.id.nav_drawer_layout)
+
+        val toogle: ActionBarDrawerToggle = ActionBarDrawerToggle(this,drawerLayout, toolbar,
+                    R.string.toogle_open,R.string.toogle_close)
+        drawerLayout.addDrawerListener(toogle)
+
+        toogle.syncState()
+
+        navigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
     }
 
     private fun insertListeners (){
@@ -21,5 +42,9 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, ProductDetailActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("Not yet implemented")
     }
 }
