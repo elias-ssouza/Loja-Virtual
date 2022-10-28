a1a926d5f81d766bf19d845877ee09c5ae1e2cb4
elias-ssouza <elias.ssouza@hotmail.com>
2022-10-28 02:35:28 -0300

    alteração na activity_main.xml e criação do model e adapter de categoria do produto

--- app/src/main/java/com/example/lojavirtual/MainActivity.kt
+++ app/src/main/java/com/example/lojavirtual/MainActivity.kt
@@ -3,40 +3,17 @@ package com.example.lojavirtual
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import android.widget.Toast
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.widget.Toolbar
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.Navigation
 import com.example.lojavirtual.databinding.ActivityMainBinding
-import com.google.android.material.navigation.NavigationView
 
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity() {
 
-    lateinit var toolbar: Toolbar
-    lateinit var drawerLayout: DrawerLayout
-    lateinit var navigationView: NavigationView
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         insertListeners()
-
-        toolbar = findViewById(R.id.toolbar)
-        drawerLayout = findViewById(R.id.nav_drawer_layout)
-
-        val toogle: ActionBarDrawerToggle = ActionBarDrawerToggle(this,drawerLayout, toolbar,
-                    R.string.toogle_open,R.string.toogle_close)
-        drawerLayout.addDrawerListener(toogle)
-
-        toogle.syncState()
-
-        navigationView = findViewById(R.id.nav_view)
-        navigationView.setNavigationItemSelectedListener(this)
     }
 
     private fun insertListeners (){
@@ -44,27 +21,5 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             val intent = Intent(this, ProductDetailActivity::class.java)
             startActivity(intent)
         }
-    }
-
-    override fun onBackPressed(){
-        if (drawerLayout.isDrawerOpen(GravityCompat.START))
-            drawerLayout.closeDrawer(GravityCompat.START)
-        else
-            super.onBackPressed()
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-
-        when (item.itemId) {
-            R.id.nav_home -> Toast.makeText(this, "Página Inicial", Toast.LENGTH_LONG).show()
-            R.id.nav_category -> Toast.makeText(this, "Categoria", Toast.LENGTH_LONG).show()
-            R.id.nav_orders -> Toast.makeText(this, "Meus Pedidos", Toast.LENGTH_LONG).show()
-            R.id.nav_cart -> Toast.makeText(this, "Meu Carrinho", Toast.LENGTH_LONG).show()
-            R.id.nav_logout -> Toast.makeText(this, "Sair", Toast.LENGTH_LONG).show()
-        }
-
-        drawerLayout.closeDrawer(GravityCompat.START)
-
-        return true
     }
 }
